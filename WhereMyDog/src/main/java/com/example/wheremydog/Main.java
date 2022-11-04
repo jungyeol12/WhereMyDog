5c1971e18ea8ef9ec551cde0a1af70ec6c25a719
jinpc777 <jinpc777@naver.com>
2022-11-04 11:36:00 +0900

    메인메뉴 추가
    main-menu 틀 제작 및 이미지 삽입

--- WhereMyDog/src/main/java/com/example/wheremydog/Main.java
+++ WhereMyDog/src/main/java/com/example/wheremydog/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("나의 주인은 어디에?");
+        stage.setTitle("나의 주인은 어디에?"); // 제목
         stage.setScene(scene);
         stage.show();
     }
