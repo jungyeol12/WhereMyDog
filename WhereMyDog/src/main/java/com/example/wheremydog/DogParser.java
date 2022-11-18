6ac21dd75318741d749d9a8f687fdb141b472291
jungyeol12 <jbae23233@gmail.com>
2022-11-18 12:26:01 +0900

    일부 유기 동물 표시
    일부 유기 동물 데이터를 fxml에 setText하여 표시하였다.

--- WhereMyDog/src/main/java/com/example/wheremydog/DogParser.java
+++ WhereMyDog/src/main/java/com/example/wheremydog/DogParser.java
@@ -4,6 +4,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -15,6 +17,31 @@ import java.io.IOException;
 public class DogParser {
 
     String url = "http://apis.data.go.kr/6300000/animalDaejeonService/animalDaejeonList?serviceKey=4FBaXYcryy2%2BcgNgH6SaCqRpCF3fiTUPsozPirpZl07c5%2F36hX68PScjfFM9TCL%2FpErXVA6E1HhyL%2F673SSdEA%3D%3D&numOfRows=10&pageNo=1";
+
+    @FXML
+    private Label age;
+    @FXML
+    private Label gender;
+    @FXML
+    private Label classification;
+    @FXML
+    private Label foundPlace;
+    @FXML
+    private Label species;
+    @FXML
+    private Label weight;
+
+
+    @FXML
+    private Label memo; // 특이 사항
+    @FXML
+    private Label adoptionStatusCd; // 입양 상태
+    @FXML
+    private Label animalSeq; // 게시물 번호
+    @FXML
+    private Label noticeDate; // 공고일
+    @FXML
+    private Label regDtTm; // 게시글 등록일
 
     // tag값의 정보를 가져오는 메소드
     private static String getTagValue(String tag, Element eElement) {
@@ -48,9 +75,21 @@ public class DogParser {
                 System.out.println("발견 장소  : " + getTagValue("foundPlace", eElement));
                 System.out.println("품종 : " + getTagValue("species", eElement));
                 System.out.println("몸무게  : " + getTagValue("weight", eElement));
-                System.out.println("특이사항  : " + getTagValue("memo", eElement));
-                System.out.println("구조일  : " + getTagValue("rescueDate", eElement));
-                // System.out.println("이미지  : " + getTagValue("filePath", eElement));
+
+                // Pane 에 표시될 기본 정보들
+                animalSeq.setText(getTagValue("animalSeq", eElement));
+                regDtTm.setText(getTagValue("regDtTm", eElement));
+                adoptionStatusCd.setText(getTagValue("adoptionStatusCd", eElement));
+                noticeDate.setText(getTagValue("noticeDate", eElement));
+                memo.setText(getTagValue("memo", eElement));
+
+
+                //age.setText("나이: " + getTagValue("age", eElement));
+                //gender.setText("성별: " + getTagValue("gender", eElement)); // 1: 암 2: 수
+                //classification.setText("종류  : " + getTagValue("classification", eElement)); // 1: 개 2: 고양이 3: 기타동물
+                //foundPlace.setText("발견 장소  : " + getTagValue("foundPlace", eElement));
+                //species.setText("품종 : " + getTagValue("species", eElement));
+                //System.out.println("몸무게  : " + getTagValue("weight", eElement));
             }	// for end
         }	// if end
     }
