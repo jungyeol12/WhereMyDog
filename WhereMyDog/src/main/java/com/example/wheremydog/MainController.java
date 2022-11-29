b65909a47486cc1fbed66128be5f61f29db14e33
domin03 <111326974+doomin03@users.noreply.github.com>
2022-11-29 11:40:08 +0900

    u

--- WhereMyDog/src/main/java/com/example/wheremydog/MainController.java
+++ WhereMyDog/src/main/java/com/example/wheremydog/MainController.java
@@ -1,17 +1,6 @@
 package com.example.wheremydog;
 
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import java.io.IOException;
-
 public class MainController {
 
 
